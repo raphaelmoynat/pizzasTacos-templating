4d3359dfa9af444e8dc9b1efa2a91336eeb72fda
esdb1 <pierre.lacognata@mail-esd.com>
2023-11-27 16:13:09 +0100

    avec devoirs

--- ficheTacos.php
+++ ficheTacos.php
@@ -1,2 +1,8 @@
 <?php
 //afficher un seul tacos
+
+//refaire la meme chose PLUSIEURS FOIS avec d'autres themes (sandiwich, desserts, boissons, etc)
+
+// Créer une base de données, table pizzas, ajouter des pizzas, et utilisateur (privileges)
+// PDO  : essayer de récuperer les pizzas de la BDD pour les utiliser dans l'application
+//plutot que la variable du tableau des pizzas
